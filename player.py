c26ad60d7075c4b35aca28b1ec66061ac0125839
Peter <pbezzeg@gmail.com>
2018-10-01 14:03:05 +0000

    3.6

--- player.py
+++ player.py
@@ -6,7 +6,32 @@ class Player:
     VERSION = "3.2"
 
     def betRequest(self, game_state):
+        position = game_state["in_action"]
+        players = game_state["players"]
+        community_cards = game_state["community_cards"]
 
+        def check_our_pairs():
+            first_card = own_card(0)
+            second_card = own_card(1)
+            if first_card == second_card:
+                print("pair in hand", file=sys.stderr)
+                return 100
+
+            for community_card in community_cards:
+                if (community_card['rank'] == first_card["rank"]) or (community_card['rank'] == second_card["rank"]):
+                    print("pair with desk", file=sys.stderr)
+                    return 50
+
+        def own_card(card):
+            for player in players:
+                if player["id"] == position:
+                    cards = player["hole_cards"]
+                    return cards[card]
+
+        print("asd" + str(own_card(0)), file=sys.stderr)
+        print("asd" + str(own_card(1)), file=sys.stderr)
+        
+        check_our_pairs()
         return 1000
 
     def showdown(self, game_state):
