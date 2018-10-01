0751a3c123d014320ecd33be5d844b9ba3150648
Peter <pbezzeg@gmail.com>
2018-10-01 09:25:42 +0000

    ownhand check

--- player.py
+++ player.py
@@ -1,10 +1,17 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "1.1.3"
 
     def betRequest(self, game_state):
-        
-        return 100
+
+        def owncard():
+            position = game_state["in_action"]
+            players = game_state["players"]
+            for player in players:
+                if player["id"] == position:
+                    return player["hole_cards"]
+        print owncard()
+        return 1
 
     def showdown(self, game_state):
         pass
