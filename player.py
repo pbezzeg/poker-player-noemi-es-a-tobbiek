80023db590c61c53d0eebf5b594c25eca3d9ec78
Peter <pbezzeg@gmail.com>
2018-10-01 13:42:56 +0000

    3.2

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import sys
 
 
 class Player:
-    VERSION = "3.1"
+    VERSION = "3.2"
 
     def betRequest(self, game_state):
 
@@ -116,8 +116,8 @@ class Player:
 
         print("asd" + str(community_cards), file=sys.stderr)
         print("asd" + community_cards[0]['rank'], file=sys.stderr)
-        allin()
-        check_our_pairs()
+        #allin()
+        #check_our_pairs()
         return 0
 
     def showdown(self, game_state):
