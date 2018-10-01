bb6f07f8289bd9269c2085dbc530988d87483012
Janos Marton <janosmarton22@gmail.com>
2018-10-01 10:43:11 +0200

    version: 1.1.2

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
 
 class Player:
-    VERSION = "1.1"
+    VERSION = "1.1.2"
 
     def betRequest(self, game_state):
         
-        return 100
+        return 1
 
     def showdown(self, game_state):
         pass
