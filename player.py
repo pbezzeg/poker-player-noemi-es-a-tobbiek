0fad8d7c5344306068827ee5c1fc2ae518279d6b
Peter <pbezzeg@gmail.com>
2018-10-01 14:38:26 +0000

    4.0

--- player.py
+++ player.py
@@ -3,37 +3,33 @@ import sys
 
 
 class Player:
-    VERSION = "3.8"
+    VERSION = "4"
 
     def betRequest(self, game_state):
         position = game_state["in_action"]
         players = game_state["players"]
         community_cards = game_state["community_cards"]
+        our_money = 0
+        for player in players:
+            if player["id"] == position:
+                our_money = player["stack"]
+
+        def raise_money(perc):
+            return game_state["current_buy_in"] + (our_money - game_state["current_buy_in"])/100*perc
 
         def check_our_pairs():
             first_card = own_card(0)
             second_card = own_card(1)
             if first_card == second_card:
                 print("pair in hand", file=sys.stderr)
-                return 100
+
+                return raise_money(50)
 
             for community_card in community_cards:
                 if (community_card['rank'] == first_card["rank"]) or (community_card['rank'] == second_card["rank"]):
                     print("pair with desk", file=sys.stderr)
-                    return 50
+                    return raise_money(20)
 
-
-        def allin():
-            our_money = 0
-            for player in players:
-                if player["id"] == position:
-                    our_money = player["stack"]
-
-            for player in players:
-                if (our_money < player["stack"]) and game_state["round"] > 0:
-                    return 0
-                else:
-                    return int(our_money)
 
 
         def own_card(card):
@@ -46,12 +42,9 @@ class Player:
         print("asd" + str(own_card(1)), file=sys.stderr)
 
 
-        if allin() > 0:
-            return allin()
-
         if check_our_pairs() > 49:
             return check_our_pairs()
-        return 1000
+        return 0
 
     def showdown(self, game_state):
         pass
